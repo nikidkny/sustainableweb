d30ad50d2687eaddf794994b4299c11c600f29a7
Karina Valens <karyvalens@yahoo.com>
2022-11-21 22:37:46 +0100

    adding output to the form

--- components/Container.js
+++ components/Container.js
@@ -1,5 +1,5 @@
 import Intro from "../components/Intro";
-import Form from "../components/Form";
+//import Form from "../components/Form";
 import GettingCo2 from "../components/GettingCo2";
 import GettingKwh from "../components/GettingKwh";
 import GettingKm from "../components/GettingKm";
@@ -7,28 +7,33 @@ import GettingTrees from "../components/GettingTrees";
 import { useState } from "react";
 
 export default function Container() {
- 
+ // 1.- Declare a state variable that tracks the value of the input field.
+ // 2.- Add an onChange prop to the input field.(Inside the input HTML tag)
   const [hour, setHour] = useState('');
   const handleChange = event => {
+  // 3.- Use event.target.value to get the input field's value and update the state variable.
     setHour(event.target.value);
-    //console.log('value is:', event.target.value);
   };
+
 const month=30*hour;
-console.log(month); 
+
   return (
-    <div>
+    <>
       <Intro />
+     
+      <form>
       <input
         type="range"
-        id="message"
-        name="message"
+        id="hour"
+        name="hour"
         min="0"
         max="10"
         onChange={handleChange}
         value={hour}
       />
-      <h2>{hour} hour daily</h2>
-      <h2 month={month}>{month} hour month</h2>
+      <output for="hour" onforminput="value = foo.valueAsNumber;">{hour} hours per day</output></form>
+
+      <h2>{month} hour month</h2>
       
       <GettingKwh month={month}/>
  
@@ -37,7 +42,7 @@ console.log(month);
       <GettingCo2 month={month} />
 
       <GettingTrees />
-    </div>
+    </>
   );  
     
   
