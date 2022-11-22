59b718d184f886e82fe3375867905efa02e95c16
Karina Valens <karyvalens@yahoo.com>
2022-11-22 13:43:18 +0100

    rounding the resoults

--- components/GettingCo2.js
+++ components/GettingCo2.js
@@ -5,18 +5,11 @@ export default function GettingCo2({month}) {
   const totalCO2=1.89/* total= 1.89kgco2 per month per person */
   const totalTrees=53.33 /* total of tress per mont to clean totalCO2 */
 
-  const kgco2Month=month*0.233;/* user production c02 per month by watching netflix*/
+  const kgco2Month=Math.round((month*0.233)*100)/100;/* user production c02 per month by watching netflix*/
   
-  const trees=(kgco2Month/totalTrees)*totalCO2 /*netflix is x% of usage related with the total of co2 produce per person per month */
-const treesM=Math.ceil(trees)
 
-  console.log(kgco2Month,"co2 in use");
-  console.log(trees,"tree per month")
-  console.log(treesM,"tree per monthM")
-
-
-  return <>
+   return <>
   <h2>That means that the amount of trees to process your  
-  <h2>{kgco2Month}kgCo2</h2> emition for watching {month} hours of Netflix a month is: {treesM} tree(s) </h2>
+  <h2>{kgco2Month}kgCo2</h2> emition for watching {month} hours of Netflix a month is: {Math.ceil((kgco2Month/totalTrees)*totalCO2 )} tree(s) </h2>
     </>; 
 }
--- components/GettingKwh.js
+++ components/GettingKwh.js
@@ -1,6 +1,10 @@
 export default function GettingKwh({month}) {
+const kwhHour=0.8002;
+const kwhMonth=Math.round((month * 0.8002)*100)/100;
+
+
   return (<>Kwh
   <h2>That monthly equals to</h2>
-  <h2>{month * 0.8002}kWh</h2>
+  <h2>{kwhMonth}kWh</h2>
   </>);
 }
