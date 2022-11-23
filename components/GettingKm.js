2755b76fc4412e963273d1328238b8f8f7b9ead9
Karina Valens <karyvalens@yahoo.com>
2022-11-23 10:10:00 +0100

    solving merging conflicts

--- components/GettingKm.js
+++ components/GettingKm.js
@@ -1,15 +1,6 @@
-export default function GettingKm({ month }) {
-  return (
-    <>
-      <div className="km">
-        <p>And with that amount of energy a electric car can travel</p>
-        <h2>{month * 4.5}km</h2>;
-      </div>
-    </>
-  );
 export default function GettingKm({month}) {
 
-  const kmHour=4.5
+  const kmHour=4.5;
   return   <> 
   <h2>And with that amount of energy a electric car can travel</h2>
   <h2>{month * kmHour} km</h2>
